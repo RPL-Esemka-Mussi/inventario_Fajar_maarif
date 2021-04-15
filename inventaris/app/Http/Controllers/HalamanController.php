3fb610dafcc72f25c6938fdc161bbb21d7bc69c0
fajarmaarif1 <fjrmrf1@gmail.com>
2021-04-15 16:34:06 +0700

    first commit

---
+++ inventaris/app/Http/Controllers/HalamanController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Barang;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HalamanController extends Controller
+{
+    public function halamanlogin()
+    {
+        return view('login');
+    }
+
+
+    public function proseslogin(Request $res)
+    {
+        if (Auth::attempt($res->only(['email', 'password']))) {
+            return redirect('dashboard');
+        } else {
+            return redirect('/')->with('login', 'Email atau Password salah');
+        }
+    }
+
+
+    public function dashboard()
+    {
+        $barang = Barang::count();
+        $user = User::count();
+        return view('dashboard', [
+            'barang' => $barang,
+            'user' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/')->with('logout', 'Please Login');
+    }
+}
